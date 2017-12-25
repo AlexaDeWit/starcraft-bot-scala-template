03c603643bfc0f50523be19edc60bafb74b71528
Alexandra DeWit <alexa.dewit@gmail.com>
2017-12-25 23:12:03 +0100

    Cleanup git to how I want it working

--- build.sbt
+++ build.sbt
@@ -1,6 +1,9 @@
-name := "ScalaBot"
+name := "Broodwar Bot, Scala!"
 
 version := "1.0"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.12.4"
+
+unmanagedBase := baseDirectory.value / "lib"
+unmanagedJars in Compile := (baseDirectory.value ** "*.jar").classpath
     
--- src/main/scala/TestBot.scala
+++ src/main/scala/TestBot.scala
@@ -27,8 +27,8 @@ class TestBot extends DefaultBWListener {
         //Use BWTA to analyze map
         //This may take a few minutes if the map is processed first time!
         System.out.println("Analyzing map...")
-        BWTA.readMap()
-        BWTA.analyze()
+        //BWTA.readMap()
+        //BWTA.analyze()
         System.out.println("Map data ready")
     }
 
